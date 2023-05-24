b1d6d10d7cf783fa978666e93e33d291c66fe428
mayanem <mayanemesquitta@gmail.com>
2023-05-24 17:09:06 -0300

    add view company

--- devsign_app/views/company/view.py
+++ devsign_app/views/company/view.py
@@ -1,9 +1,13 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from devsign_app.models import Company
 from devsign_app.serializers.company.serializer import CompanySerializer
 
 
-class CompanyList(generics.ListCreateAPIView):
-    queryset = Company.objects.all()
-    serializer_class = CompanySerializer
+class CompanyAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        result = Company.objects.all()
+        serializer = CompanySerializer(result, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
--- devsign_back/urls.py
+++ devsign_back/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from devsign_app.views.company import CompanyAPIView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('company', CompanyAPIView.get())
 ]
